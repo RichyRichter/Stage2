e38ff158acd0532ecce0aad43f1d2f7a1fc6505d
RichyRichter <like333me@yandex.ru>
2022-03-27 14:39:38 +0400

    dataBase task added

--- src/main/java/stage2/demo/App.java
+++ src/main/java/stage2/demo/App.java
@@ -3,28 +3,101 @@ package stage2.demo;
 import stage2.practice.DataBase.ConnectionManager;
 import stage2.practice.DataBase.Controller;
 
-import java.sql.ResultSet;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Statement stat = ConnectionManager.getStatement();
-        Controller controller = new Controller(stat);
-        controller.addCenter(5,"Оренбург", 1, 250, 850000, 1805);
 
-        try (ResultSet rs = stat.executeQuery("select * from POPULATION_CENTER order by ID_CENTER ")) {
-            while (rs.next()) {
-                System.out.print(rs.getString(1) + "|");
-                System.out.print(rs.getString(2) + "\n");
-                System.out.print(rs.getString(3) + "\n");
-                System.out.print(rs.getString(4) + "\n");
-                System.out.print(rs.getString(5) + "\n");
-                System.out.print(rs.getString(6) + "\n");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+        Scanner scanner = new Scanner(System.in);
+        Controller controller = new Controller(ConnectionManager.getStatement());
+        StringBuilder res = new StringBuilder();
+        System.out.println(controller.findAllCenters());
+        System.out.println("Выберите действие:\n" +
+                "1 - Создать/удалить пункт или улицу\n" +
+                "2 - Получить информацию о населенном пункте\n" +
+                "3 - Получить информацию о наличии улицы\n" +
+                "4 - Получить информацию по критериям поиска\n");
+
+
+        switch (scanner.nextInt()) {
+            case 1 -> changePlace();
+            case 2 -> showCenterInfo(scanner, controller, res);
+            case 3 -> showStreetInfo(scanner, controller, res);
+            case 4 -> infoByParameter(scanner, controller, res);
         }
         ConnectionManager.closeConnection();
+    }
+
+    private static void infoByParameter(Scanner scanner, Controller controller, StringBuilder res) throws SQLException, IOException {
+        System.out.println("Напишите критерий поиска, условие и значение через пробел. " +
+                "Примеры: Население >= 100000.\nВозможные критерии: Население, Год, Площадь.");
+        res.append(controller.findCenterByCondition(scanner.next(), scanner.next(), scanner.nextInt()));
+        System.out.println(res);
+        writeFile(scanner, res);
+    }
+
+    private static void showStreetInfo(Scanner scanner, Controller controller, StringBuilder res) throws SQLException, IOException {
+        System.out.println("Напишите название или часть названия улицы. Примеры: Сол, Солнечная\n");
+        res.append(controller.findStreetInCenter(scanner.next()));
+        System.out.println(res);
+        writeFile(scanner, res);
+    }
+
+    private static void showCenterInfo(Scanner scanner, Controller controller, StringBuilder res) throws SQLException, IOException {
+        System.out.println("Напишите название населенного пункта\n");
+        res.append(controller.findCenterInfo(scanner.next()));
+        System.out.println(res);
+        writeFile(scanner, res);
+    }
+
+    private static void writeFile(Scanner scanner, StringBuilder res) throws IOException {
+        System.out.println("Сохранить данные в файл?\n" +
+                "1 - Да 2 - Нет");
+        if (scanner.nextInt() == 1) {
+            writeFile(res);
+        }
+    }
+
+    private static void writeFile(StringBuilder res) throws IOException {
+        File result = new File("src/main/java/stage2/practice/DataBase/result.txt");
+        if (!result.exists()) result.createNewFile();
+        PrintWriter printWriter = new PrintWriter("src/main/java/stage2/practice/DataBase/result.txt");
+        printWriter.print(res.toString());
+        printWriter.close();
+    }
+
+    private static void changePlace() throws SQLException {
+        Controller controller = new Controller(ConnectionManager.getStatement());
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выберите действие:\n" +
+                "1 - Создать населенный пункт\n" +
+                "2 - Создать улицу\n" +
+                "3 - Удалить населенный пункт или улицу\n" +
+                "4 - Добавить улицу в город");
+        switch (scanner.nextInt()) {
+            case 1 -> {
+                System.out.println("Введите данные о населенном пунте через пробел. Пример - Самара Город 541 1144000 1586");
+                controller.addCenter(scanner.next(), scanner.next(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+            }
+            case 2 -> {
+                System.out.println("Введите данные о улице через пробел. Пример - Солнечная Улица. Виды улиц: Улица, " +
+                        "Бульвар, Проспект, Аллея, Набережная. ");
+                controller.addStreet(scanner.next(), scanner.next());
+            }
+            case 3 -> {
+                System.out.println("Введите данные для удаления через пробел. Примеры: Москва Пункт, Длинная Улица");
+                controller.deletePlace(scanner.next(), scanner.next());
+            }
+            case 4 -> {
+                System.out.println(controller.findAllStreets());
+                System.out.println("Укажите название города и ID улицы через пробел. Пример: Москва 2");
+                controller.addStreetToCenter(scanner.next(), scanner.nextInt());
+            }
+        }
     }
 }
--- src/main/java/stage2/practice/DataBase/Controller.java
+++ src/main/java/stage2/practice/DataBase/Controller.java
@@ -1,5 +1,6 @@
 package stage2.practice.DataBase;
 
+import java.security.PublicKey;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -7,23 +8,159 @@ import java.sql.Statement;
 public class Controller {
     private Statement stat;
 
-    public Controller(Statement stat) {
+    public Controller(Statement stat) throws SQLException {
         this.stat = stat;
     }
 
-    public void addCenter(int id, String name, int type, int area, int population, int fondYear) throws SQLException {
-        try(ResultSet rs = stat.executeQuery("select * from POPULATION_CENTER")){
-            rs.moveToInsertRow();
-            rs.updateInt(1,id);
-            rs.updateString(2,name);
-            rs.updateInt(3,type);
-            rs.updateInt(4,area);
-            rs.updateInt(5,population);
-            rs.updateInt(6,fondYear);
-            rs.insertRow();
-            rs.moveToCurrentRow();
+    public boolean addStreetToCenter(String centerName, int IdStreet) throws SQLException {
+        String queryCenter = String.format("SELECT ID_CENTER from POPULATION_CENTER " +
+                "where CENTER_NAME = '%s'", centerName);
+        ResultSet set = stat.executeQuery(queryCenter);
+        set.next();
+        int idTCenter = set.getInt(1);
+        return stat.execute(String.format("insert into CENTER_STREET\n" +
+                "values (%d, %d);", idTCenter, IdStreet));
+    }
+
+    public boolean addCenter(String name, String type, int area, int population, int fondYear) throws SQLException {
+        if (area <= 0 || population <= 0 || fondYear <= 0) {
+            throw new IllegalArgumentException("Отрицательные значения");
         }
-//        return stat.execute("insert into population_center (center_name, id_type, CITY_AREA, POPULATION, FOUNDATION_YEAR)\n" +
-//                "values ('" + name + "', " + type + ", " + area + ", " + population + ", " + fondYear + ")");
+        String query = String.format("SELECT ID_CENTER_TYPE from CENTER_TYPE " +
+                "where CENTER_TYPE_NAME = '%s'", type);
+        ResultSet set = stat.executeQuery(query);
+        set.next();
+        int idType = set.getInt(1);
+        return stat.execute(String.format("insert into population_center (center_name, id_type, CITY_AREA, POPULATION, " +
+                "FOUNDATION_YEAR) values ('%s', %d, %d, %d, %d)", name, idType, area, population, fondYear));
+    }
+
+    public boolean deletePlace(String placeName, String placeType) throws SQLException {
+        String colum;
+        switch (placeType) {
+            case "Пункт" -> {
+                placeType = "POPULATION_CENTER";
+                colum = "CENTER_NAME";
+            }
+            case "Улица" -> {
+                placeType = "STREET";
+                colum = "STREET_NAME";
+            }
+            default -> throw new IllegalStateException("Unexpected value: " + placeType);
+        }
+        String query = String.format("delete from %s " +
+                "where %s = '%s'", placeType, colum, placeName);
+        return stat.execute(query);
+    }
+
+    public String findAllCenters() throws SQLException {
+        StringBuilder res = new StringBuilder("Населенные пункты:\n");
+        String query = "select * from centerView";
+        try (ResultSet rs = stat.executeQuery(query)) {
+            while (rs.next()) {
+                res.append(String.format("%s | %s | %s км2 | %s чел. | %s г.\n",
+                        rs.getString(1), rs.getString(2), rs.getString(3),
+                        rs.getString(4), rs.getString(5)));
+            }
+        }
+        return res.toString();
+    }
+
+    public String findAllStreets() throws SQLException {
+        StringBuilder res = new StringBuilder("Улицы:\n");
+        String query = "select ID_STREET, STREET_NAME, STREET_TYPE_NAME\n" +
+                "from STREET\n" +
+                "join STREET_TYPE ST on ST.ID_STREET_TYPE = STREET.ID_STREET_TYPE";
+        try (ResultSet rs = stat.executeQuery(query)) {
+            while (rs.next()) {
+                res.append(String.format("%s | %s | %s \n",
+                        rs.getString(1), rs.getString(2), rs.getString(3)));
+            }
+        }
+        return res.toString();
+    }
+
+    public boolean addStreet(String name, String type) throws SQLException {
+        String query = String.format("SELECT ID_STREET_TYPE from STREET_TYPE " +
+                "where STREET_TYPE_NAME = '%s'", type);
+        ResultSet set = stat.executeQuery(query);
+        set.next();
+        int idType = set.getInt(1);
+        query = String.format("insert into STREET (street_name, id_street_type) " +
+                "VALUES ('%s',%d)", name, idType);
+        return stat.execute(query);
+    }
+
+    public String findStreetInfo(String streetName) throws SQLException {
+        StringBuilder res = new StringBuilder();
+        String query = "select STREET.id_street, STREET_TYPE_NAME, STREET.street_name\n" +
+                "from STREET\n" +
+                "         join STREET_TYPE ST on ST.ID_STREET_TYPE = STREET.ID_STREET_TYPE\n" +
+                "where STREET_NAME = '" + streetName + "'";
+        try (ResultSet rs = stat.executeQuery(query)) {
+            while (rs.next()) {
+                res.append(String.format("%s | %s | %s \n",
+                        rs.getString(1), rs.getString(2), rs.getString(3)));
+            }
+        }
+        return res.toString();
+    }
+
+    public String findCenterInfo(String centerName) throws SQLException {
+        String query = "select CENTER_NAME, ct.CENTER_TYPE_NAME, CITY_AREA, POPULATION, FOUNDATION_YEAR\n" +
+                "from POPULATION_CENTER\n" +
+                "join CENTER_TYPE ct on ct.ID_CENTER_TYPE = ID_TYPE\n" +
+                "where CENTER_NAME = '" + centerName + "'";
+        StringBuilder res = new StringBuilder("По названию (" + centerName + ") найден населенный пункт:\n");
+        try (ResultSet rs = stat.executeQuery(query)) {
+            while (rs.next()) {
+                res.append(String.format("%s | %s | %s км2 | %s чел. | %s г.\n",
+                        rs.getString(1), rs.getString(2), rs.getString(3),
+                        rs.getString(4), rs.getString(5)));
+            }
+        }
+        res.append("Улицы:\n");
+        query = "select STREET_TYPE_NAME, STREET_NAME from centerStreets where CENTER_NAME = '" + centerName + "'";
+        try (ResultSet rs = stat.executeQuery(query)) {
+            while (rs.next()) {
+                res.append(String.format("%s | %s \n",
+                        rs.getString(1), rs.getString(2)));
+            }
+        }
+        return res.toString();
+    }
+
+    public String findStreetInCenter(String streetName) throws SQLException {
+        StringBuilder res = new StringBuilder("Улица '" + streetName + "' есть в следующих населенных пунктах:\n");
+        String query = "select STREET_NAME, CENTER_NAME from CENTER_STREET\n" +
+                "join STREET S on S.ID_STREET = CENTER_STREET.ID_STREET\n" +
+                "join POPULATION_CENTER PC on PC.ID_CENTER = CENTER_STREET.ID_CENTER\n" +
+                "where STREET_NAME like '%" + streetName + "%'";
+        try (ResultSet rs = stat.executeQuery(query)) {
+            while (rs.next()) {
+                res.append(String.format("%s | %s \n", rs.getString(2), rs.getString(1)));
+            }
+        }
+        return res.toString();
+    }
+
+    public String findCenterByCondition(String parameter, String condition, int num) throws SQLException {
+        StringBuilder res = new StringBuilder("Населенные пункты подходящие под условие " +
+                "(" + parameter + condition + num + "):\n");
+        switch (parameter) {
+            case "Население" -> parameter = "population";
+            case "Год" -> parameter = "foundation_year";
+            case "Площадь" -> parameter = "city_area";
+            default -> throw new IllegalStateException("Unexpected value: " + parameter);
+        }
+        String query = "select CENTER_NAME\n" +
+                "from population_center\n" +
+                "where " + parameter + condition + num;
+        try (ResultSet rs = stat.executeQuery(query)) {
+            while (rs.next()) {
+                res.append(String.format("%s \n", rs.getString(1)));
+            }
+        }
+        return res.toString();
     }
 }
